a70f8c7dde9f717ccc05384c1ee297b21e540fec
Kieran Hewitson <kieran.hewitson@ao.com>
2022-01-27 21:03:09 +0000

    initial commit

---
+++ Logic.cs
@@ -0,0 +1,30 @@
+﻿namespace SeeSawChallenge.Incomplete
+{
+    public class Logic
+    {
+        private readonly ISeeSaw _seeSaw;
+
+        public Logic(ISeeSaw seeSaw)
+        {
+            _seeSaw = seeSaw;
+        }
+
+        public Guess Guess()
+        {
+            var result = _seeSaw.Balance(new[] { "A" }, new[] { "L" });
+
+            return new Guess("C", false);
+        }
+    }
+
+    public class Guess
+    {
+        public Guess(string person, bool isHeavier)
+        {
+            Person = person;
+            IsHeavier = isHeavier;
+        }
+        public string Person { get; }
+        public bool IsHeavier { get; }
+    }
+}
---
+++ Tests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace SeeSawChallenge.Incomplete
+{
+    public class Tests
+    {
+        private SeeSaw _seeSaw;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _seeSaw = new SeeSaw();
+        }
+
+        [Test]
+        public void Guess_Correctly()
+        {
+            var guessLogic = new Logic(_seeSaw);
+
+            var guess = guessLogic.Guess();
+
+            Assert.That(_seeSaw.Guess(guess.Person, guess.IsHeavier));
+        }
+    }
+}
